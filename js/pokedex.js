5a18e539ebccfc4f7f3afd2ee736e8c2fcd6dd98
Brandon Haupt <haupt.brandon@gmail.com>
2022-10-22 11:39:58 -0500

    Updated js, css, index
    - changed html to better allow for information
    - changed js to append more info to the DOM
    - changed css to add better styling

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,24 +1,45 @@
-const pokemonURL = "https://dummydata.netlify.app/pokedex.json"
+const defaultURL = "https://pokeapi.co/api/v2/pokemon/"
 
 // console.log(pokemonURL)
 
 function pokeSearch(pokemon) {
 
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    const url = `${defaultURL}${pokemon}`
 
     $.ajax(url).then((poke) => {
         //example on how to get the pokemon names/ids/etc
-        console.log(poke.sprites.front_default)
+        // console.log(poke.sprites.front_default)
+        console.log(poke)
+        console.log(poke.id)
+        console.log(poke.types[0].type.name)
 
         const $main = $('main')
         $main.empty()
 
         const div = $('<div>')
 
-        div.html(`<img src="${poke.sprites.front_default}"></img>`)
+        div.html(`<img id="pokemonImg" src="${poke.sprites.front_default}"></img>
+        <div class="pokemonInfo">
+            <p class="pokeName">Name: ${poke.name}</p>
+            <p class="pokeId">ID: ${poke.id}</p>
+            <p class="pokeWeight">Weight: ${poke.weight}</p>
+            <p class="pokeType">Type: ${poke.types[0].type.name}</p>
+        </div>
+            
+        `)
 
         $main.append(div)
     })
 }
 
-pokeSearch("ditto")
+// Allowing users to choose what pokemon they want to look for
+$('input[type=submit]').on("click", (event) => {
+    event.preventDefault()
+
+    const inputText = $('input[type=text').val().toLowerCase()
+
+    pokeSearch(inputText)
+
+})
+
+pokeSearch("squirtle")
